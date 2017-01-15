a1e21c0be14a7d2f3be64d430846a162f6b824dc
Muigai <muigai@Muigais-MacBook-Air.local>
2017-01-15 11:29:44 -0500

    updated procfile

--- server.js
+++ server.js
@@ -1,4 +1,13 @@
 var express = require('express');
+var mongoose = require('mongoose');
+
+var url = process.env.MONGOLAB_URI || 'mongodb://localhost:27017/test';
+
+// Connect to the DB
+mongoose.connect(url);
+
+//var db = mongoose.connection;
+
 var app = express();
 
 var bodyParser = require('body-parser');
@@ -9,6 +18,30 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(__dirname + '/public'));
 
 require ("./test/app.js")(app);
+
+/*
+// require the mongodb native drivers.
+var mongodb = require('mongodb');
+
+var MongoClient = mongodb.MongoClient;
+
+// url for local database
+var url = 'mongodb://localhost:27017/test';
+
+// Use connect method to connect to the Server
+MongoClient.connect(url, function (err, db) {
+    if (err) {
+        console.log('Unable to connect to the mongoDB server. Error:', err);
+    } else {
+        console.log('Connection established to', url);
+
+        // do some work here with the database.
+
+        //Close connection
+        db.close();
+    }
+});
+*/
 
 var port = process.env.PORT || 3000;
 
