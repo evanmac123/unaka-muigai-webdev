fa0e440d1e10bd0525729e362f1a57b50eb62164
Muigai <muigai@Muigais-MacBook-Air.local>
2017-01-15 14:20:01 -0500

    update heroku config variables

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 var express = require('express');
-var mongoose = require('mongoose');
+//var mongoose = require('mongoose');
 
 // var url = process.env.MONGOLAB_URI || 'mongodb://localhost:27017/test';
 //
@@ -13,8 +13,12 @@ var mongoose = require('mongoose');
 //     console.log("Connection succeeded, connected to db instance. Url is " + url);
 // });
 var connectionString = 'mongodb://127.0.0.1:27017/test';
-if(process.env.MONGODB_TEST_ADMIN){
-    var connectionString = process.env.MONGODB_TEST_ADMIN;
+if(process.env.MLAB_USERNAME) {
+    connectionString = process.env.MLAB_USERNAME + ":" +
+        process.env.MLAB_PASSWORD + "@" +
+        process.env.MLAB_HOST + ':' +
+        process.env.MLAB_PORT + '/' +
+        process.env.MLAB_APP_NAME;
 }
 
 var app = express();
